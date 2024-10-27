297dbd7948ff4bd524a6ab1efd23e8821bfc4166
Kristina Stoyanova <44975817+KristinaSS@users.noreply.github.com>
2024-10-27 10:38:52 +0200

    Fixed filter problem

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatChipSelectionChange } from "@angular/material/chips";
-import { Champion } from "../../models/champion";
-import { Account } from "../../models/account";
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatChipSelectionChange} from "@angular/material/chips";
+import {Champion} from "../../models/champion";
+import {Account} from "../../models/account";
 
 @Component({
   selector: 'app-filters',
@@ -11,7 +11,6 @@ import { Account } from "../../models/account";
 export class FiltersComponent {
   @Input() isLoading: boolean = false;  // Input for the isLoading flag
   @Input() champions: Champion[] = [];
-  @Input() clickedNum: number = 0;
   @Input() account: Account | undefined;
 
   // Two-way binding setup for 'search' property
@@ -24,6 +23,9 @@ export class FiltersComponent {
   @Input() noResults: boolean = false;
   @Output() noResultsChange = new EventEmitter<boolean>();  // Emit changes
 
+  @Input() clickedNum: number = 0;
+  @Output() clickedNumChange = new EventEmitter<number>();  // Emit changes
+
   disableSearch: boolean = false;
   isOwned: boolean = false;
   isLegacy: boolean = false;
@@ -35,16 +37,18 @@ export class FiltersComponent {
   isTranscendent: boolean = false;
 
   onChipToggle($event: MatChipSelectionChange) {
+    this.selectChip($event);
+
     // Logic for toggling filters
     this.disableSearch = this.isOwned || this.isLiked || this.isLegacy || this.isEpic || this.isLegendary || this.isMythic || this.isTranscendent || this.isUnavailable;
-    this.filterChampions();
+    this.filterChampionsByChips();
+
+    // Emit the search change back to the parent
+    this.emitChanges()
   }
 
   filteredChampionsBySearch() {
-    this.noResults = false;
-    this.filteredChampions = this.champions;
     this.resetAllChampsToggle();
-    this.clickedNum = 0;
 
     if (this.search.length > 2) {
       this.filteredChampions = this.champions.map(champion => {
@@ -56,7 +60,7 @@ export class FiltersComponent {
             return false;
           }
         );
-        return { ...champion, skins: filteredSkins };
+        return {...champion, skins: filteredSkins};
       }).filter(champion => {
         if (champion.skins.length > 0) return true;
         if (champion.name.toLowerCase().includes(this.search.toLowerCase())) return true;
@@ -70,18 +74,20 @@ export class FiltersComponent {
 
     // Emit the search change back to the parent
     this.searchChange.emit(this.search);
-    this.filteredChampionsChange.emit(this.filteredChampions);
-    this.noResultsChange.emit(this.noResults);
+    this.emitChanges()
   }
 
   private resetAllChampsToggle() {
+    this.noResults = false;
+    this.filteredChampions = this.champions;
+    this.clickedNum = 0;
     this.filteredChampions.forEach(champion => {
       champion.showOtherSkins = false;
     });
   }
 
-  private filterChampions() {
-    // Logic to filter champions based on the chip toggles
+  private filterChampionsByChips() {
+    this.resetAllChampsToggle();
     this.filteredChampions = this.champions.map(champion => {
       return {
         ...champion,
@@ -94,10 +100,50 @@ export class FiltersComponent {
           const isMythicCondition = !this.isMythic || (skin.isBase || skin.rarity === 'Mythic');
           const isTranscendentCondition = !this.isTranscendent || (skin.isBase || skin.rarity === 'Transcendent');
           const isUnavailableCondition = !this.isUnavailable || (skin.isBase || (skin.availability === 'Limited' && skin.rarity !== 'Mythic' && skin.rarity !== 'Transcendent'));
-
           return isOwnedCondition && isLikedCondition && isLegacyCondition && isEpicCondition && isLegendaryCondition && isMythicCondition && isUnavailableCondition && isTranscendentCondition;
         })
       };
     }).filter(champion => champion.skins.length > 1);
+  }
+
+  private selectChip($event: MatChipSelectionChange) {
+    const chipId = $event.source.id;
+    const isSelected = $event.selected;
+
+    // Update the state based on chip id
+    switch (chipId) {
+      case 'isOwned':
+        this.isOwned = isSelected;
+        break;
+      case 'isLiked':
+        this.isLiked = isSelected;
+        break;
+      case 'isLegacy':
+        this.isLegacy = isSelected;
+        break;
+      case 'isEpic':
+        this.isEpic = isSelected;
+        break;
+      case 'isLegendary':
+        this.isLegendary = isSelected;
+        break;
+      case 'isMythic':
+        this.isMythic = isSelected;
+        break;
+      case 'isTranscendent':
+        this.isTranscendent = isSelected;
+        break;
+      case 'isUnavailable':
+        this.isUnavailable = isSelected;
+        break;
+      default:
+        break;
+    }
+  }
+
+  private emitChanges() {
+    this.filteredChampionsChange.emit(this.filteredChampions);
+    this.noResultsChange.emit(this.noResults);
+    this.clickedNumChange.emit(this.clickedNum);
   }
 }
